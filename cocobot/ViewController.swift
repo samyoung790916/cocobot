f93680a4f3177048355095a9d7a2eeba030058bb
samyoung79 <samyoung79@samyoung79ui-macbook-pro.local>
2019-01-02 13:08:30 +0900

    Initial Commit

---
+++ cocobot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cocobot
+//
+//  Created by samyoung79 on 02/01/2019.
+//  Copyright © 2019 samyoung79. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
